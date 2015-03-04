06d252fc4ac2be2e3cae3ae6b3419cfcd8f648c8
ruthno <ruthno@gmail.com>
2015-03-03 20:02:59 -0500

    bg image change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
            session[:user_id] = user.id
            redirect_to('/about')
        else
-           redirect_to('/login')
+           redirect_to('/contact')
        end
     end
     
